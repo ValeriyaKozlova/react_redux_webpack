6c00ac08474150bcd9e680ca6f83895f7ecccb4c
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-10-28 16:27:38 +0200

    redux, rect-redux, redux-thunk installed, store created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCount } from '../reducers/reposReducer'
 import './app.less'
 
 export default function App() {
+  const dispatch = useDispatch()
+  const count = useSelector(state => state.repos.count)
+  function onCountClick() {
+    dispatch(setCount(5))
+  }
   return (
     <div className="app">
-      React is working
+      <button onClick={() => onCountClick()}>COUNT</button>
+      <div>{count}</div>
     </div>
   )
 }
